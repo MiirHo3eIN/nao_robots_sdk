dda8e5dc8ebd09e6707ac5c4eaafd22c9a7f7956
MiirHo3eIN <amirhmoallemi@gmail.com>
2023-04-04 18:06:51 +0200

    finding & tracking works

--- examples/kick_the_ball_project/02_finder_and_tracker.py
+++ examples/kick_the_ball_project/02_finder_and_tracker.py
@@ -2,115 +2,166 @@ import sys
 import time
 from naoqi import ALProxy
 
-# Configuration of the Script 
-fractionMaxSpeed_posture = 0.8
-fractionMaxSpeed_head = 0.2
-#angles = [-2.086017, -1.526988, -1.089958, -0.903033, -0.756077, -0.486074, 
-#            0.0, 
-#            0.486074, 0.756077, 0.903033, 1.089958, 1.526988, 2.086017]
 
 
-# Z - axis 
-angles = [-1.5,0,1.5]
-    
-time_list = [2.0,4.0,6.0]
-
-isAbsolute = True
-
-
-targetName = "RedBall"
-diameterOfBall = 0.15
+# It is a global port to connect to the ROBOTs. Do not change it. 
 PORT = 9559
 
 
-def ball_detector(motionProxy, tracker_lost_status): 
-    pass
+# Configuration of the Script 
+fractionMaxSpeed_posture_ = 0.8
+fractionMaxSpeed_head_ = 0.2
 
-def ball_tracker(): 
 
+# you can find all the possible angles in this link <http://doc.aldebaran.com/2-8/family/nao_technical/joints_naov6.html>
 
-    pass 
+head_angles_z_axis = [-1.526988, -0.903033, -0.756077, -0.486074, 0.0, 0.486074, 0.756077, 0.903033, 1.526988] # 9 angles
 
-def tracker_lost_status_computer(tracker_proxy): 
-    return tracker_proxy.isTargetLost()
-
-def main(robotIP):
+# Z - axis 
+#angles = [-1.5,0,1.5]
     
+time_list = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 ,9.0]
 
-    try:
-        motionProxy = ALProxy("ALMotion", robotIP, PORT)
-        posture_proxy = ALProxy("ALRobotPosture", robotIP, PORT)
-        tracker = ALProxy("ALTracker", robotIP, PORT)
-    except Exception,e:
-        print "Could not create proxy to ALProxy"
-        print "Error was: ",e
-        sys.exit(1)
+isAbsolute = True
+
+# Set the Target the headYaw to move around. 
+head_joint_name_ = "HeadYaw"
+inital_z_angle_ = 0
 
 
+# Track Motion Configuration ------------------
+
+tracker_target_name_ = "RedBall"
+
+diameter_of_ball_ = 0.15
+
+# mode of tracker in case of finding the ball
+tracker_body_mode_ = "Move"
+         
 
 
 
 
-    # Go to posture Stand
-    posture_proxy.goToPosture("StandInit", fractionMaxSpeed_posture)
 
-    # Turn the Head On. 
-    motionProxy.setStiffnesses("Head", 1.0)
 
-    # Add target to track.
-    tracker.registerTarget(targetName, diameterOfBall)
 
-    # set mode
-    mode = "Move"
-    tracker.setMode(mode)
+class ball_kicker: 
 
-    # Then, start tracker.
-    tracker.track(targetName) 
+    def __init__(self, robot_IP):
+        
+        # IP of your robot. 
+        self.robot_ip_ = robot_IP
+        try:
+            # To look for the ball
+            self.motion_proxy   = ALProxy("ALMotion", self.robot_ip_, PORT)
+            # To Change the Postures of the Robot
+            self.posture_proxy = ALProxy("ALRobotPosture", self.robot_ip_, PORT)
+            # To track the red ball 
+            self.tracker_proxy = ALProxy("ALTracker", self.robot_ip_, PORT)
+        except Exception,e:
+            print "Could not create proxy to ALProxy"
+            print "Error was: ",e
+            sys.exit(1)
 
-    angle_indx = 0
-    # Target the headYaw to move around. 
-    names = "HeadYaw"
-    angle = angles[0]
+    def initial_posistion(self):
+        
+        # Go to posture Stand
+        self.posture_proxy.goToPosture("StandInit", fractionMaxSpeed_posture_)
+        # Add target to track.
+        self.tracker_proxy.registerTarget(tracker_target_name_, diameter_of_ball_)
+        # Set the tracker mode
+        self.tracker_proxy.setMode(tracker_body_mode_)
+        # Start the tracker 
+        self.tracker_proxy.track(tracker_target_name_) 
+        # Turn Head Joints On. 
+        self.motion_proxy.setStiffnesses("Head", 1.0)
 
-    motionProxy.setAngles(names,angle,fractionMaxSpeed_head) 
-    # wait half a second
-    time.sleep(0.5)
-    next_state = 1
-    track_losst_status = 0
-     
+
+    
+    
+    def final_position(self):
+        self.tracker_proxy.stopTracker()
+        self.tracker_proxy.unregisterAllTargets()
+        self.posture_proxy.goToPosture( "Sit", fractionMaxSpeed_posture_)
+
+
+
+    def tracker_lost_status(self): 
+       return self.tracker_proxy.isTargetLost()
+
+
+
+
+    def find_ball(self): 
+        # Find the ball. Returns the status of the tracker.  
+
+        # Turn Head Joint On
+        self.motion_proxy.setStiffnesses("Head", 1.0)
+        # Set initial position of the head
+        self.motion_proxy.setAngles(head_joint_name_, inital_z_angle_ ,fractionMaxSpeed_head_)
+        
+        tracking_status_ = self.tracker_lost_status()
+        
+        for indx, z_angle_ in enumerate(head_angles_z_axis): 
+            if tracking_status_:
+                # change the angle of head joints
+                self.motion_proxy.angleInterpolation(head_joint_name_, z_angle_, time_list[indx], isAbsolute)
+                tracking_status_ = self.tracker_lost_status()
+            else:
+                # Turn Head joints off 
+                self.motion_proxy.setStiffnesses("Head", 0.0)
+                # The ball is find 
+                return tracking_status_ 
+        return tracking_status_
+    def track_ball(self):
+
+        """
+        you can find methods of ALTrack in this link <http://doc.aldebaran.com/2-8/naoqi/trackers/altracker-api.html#ALTrackerProxy>
+        """
+
+        # Start the tracker 
+        self.tracker_proxy.track(tracker_target_name_)
+        tracking_status_ = self.tracker_lost_status()
+        return tracking_status_
+
+
+
+
+def main():
+    
+    robot_IP = "192.168.0.101"
+    # Make an object of ball_kicker class. 
+    ball_kicker_instance = ball_kicker(robot_IP)
+    
+    # Get into the inital position
+    ball_kicker_instance.initial_posistion()
+
+    # Get the lost status
+    tracking_status_ = ball_kicker_instance.tracker_lost_status()
+
     try: 
         while True:
-            track_losst_status = tracker_lost_status_computer(tracker)
-            print(track_losst_status)    
-            for angle in range(0, len(angles)):
-                #print(angle)
-                motionProxy.angleInterpolation(names, angles[angle], time_list[angle], isAbsolute)
-                #time.sleep(0.5)
-                if track_losst_status:
-                    motionProxy.setStiffnesses("Head", 1.0)
-                    track_losst_status = tracker_lost_status_computer(tracker) 
-                    continue
-                else: 
-                    # Turn off the Head Stiffness.
-                    motionProxy.setStiffnesses("Head", 0.0)
-                    tracker.track(targetName)
-
-                
+            if tracking_status_: 
+                tracking_status_ = ball_kicker_instance.find_ball() 
+            else: 
+                tracking_status_ = ball_kicker_instance.track_ball()
+            
+            print(tracking_status_)
     except KeyboardInterrupt:
         print
         print "Interrupted by user"
         print "Stopping..."
 
-    #time.sleep(3.0)
+    time.sleep(3.0)
+
+    # Come back to sitting position
+    ball_kicker_instance.final_position()
 
 
-    tracker.stopTracker()
-    tracker.unregisterAllTargets()
-    posture_proxy.goToPosture( "Sit", fractionMaxSpeed_posture)
 
 if __name__ == "__main__":
-    robotIp = "192.168.0.101"
+    
 
-    main(robotIp)
+    main()
 
 
